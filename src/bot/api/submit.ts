c68f908b0a37305a20aa13abe0892477b48f7c9b
Eldar Agayev <theeldarwes@gmail.com>
2024-03-10 04:29:58 +0000

    Change of endpoint

--- src/bot/api/submit.ts
+++ src/bot/api/submit.ts
@@ -1,11 +1,11 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import fetch from "node-fetch";
 
-// type ResponseDataType = {
-//   image: string;
-//   index: number;
-//   score: object;
-// };
+interface ResponseDataType {
+  image: string;
+  index: number;
+  score: number;
+}
 
 // Updated function to submit contest data and return the API response
 export default async function submitContestData(
@@ -33,14 +33,14 @@ export default async function submitContestData(
     }
 
     // Parse the JSON response and return it
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const responseData: any = await response.json(); // Assuming the API responds with JSON
+    const responseData: ResponseDataType =
+      (await response.json()) as ResponseDataType; // Assuming the API responds with JSON
 
     // Return the parsed response
     return {
       image: responseData.image,
       index: responseData.index,
-      score: responseData.score.score,
+      score: responseData.score,
     };
   } catch {
     return { image: "", index: -1, score: -1 };
